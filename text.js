d207bcd92b6dc4944f50c1561186e8b9cc1b86ca
BabzT <babatumdehezekiah7@gmail.com>
2022-06-09 00:55:35 +0100

    commit

--- text.js
+++ text.js
@@ -1,5 +1,25 @@
 const myPosts = document.querySelector(".blogs");
 
+window.addEventListener("scroll", reveal);
+
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+    
+    for(var i = 0; i < reveals.length; i++){
+
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 50;
+
+        if (revealtop < windowheight - revealpoint) {
+            reveals[i].classList.add("active");
+        }
+        else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
 
 const renderPosts = async (term) => {
 
@@ -15,7 +35,7 @@ const renderPosts = async (term) => {
     let template = "";
     posts.forEach(post => {
         template += `
-            <div class="post">
+            <div class="post reveal">
                 <h4 class="id">${post.id}</h4>
                 <h4 class="title">${post.title}</h4>
                 <a class="readmore" href="details.html?id=${post.id}">Read more</a>
